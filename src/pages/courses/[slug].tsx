768f824f2bb0159394101f898c6f2fa5136b3646
Aman Sirohi <amansirohi077@gmail.com>
2024-03-28 16:13:39 +0530

    added back arrow

--- src/pages/courses/[slug].tsx
+++ src/pages/courses/[slug].tsx
@@ -3,8 +3,6 @@ import { courseData, CourseContent } from '@/utils/constants'; // Import your co
 import { TracingBeam } from "@/components/ui/tracing-beam";
 import "@/app/globals.css";
 import CourseSkeleton from '@/components/CourseSkeleton';
-import App from '@/components/Navbar';
-import Footer from '@/components/Footer';
 // import RootLayout from '@/app/layout';
 import Layout from '@/components/Layout';
 
@@ -16,7 +14,7 @@ const CoursePage = () => {
   const course: CourseContent | undefined = courseData.find(
     (item) => item.slug === slug
   );
- if(!course) return <div><App/><CourseSkeleton/><Footer/></div>;
+ if(!course) return <Layout><CourseSkeleton/></Layout>;
 
 
 
@@ -25,8 +23,15 @@ const CoursePage = () => {
 <div>
        <Layout title= {course.name}>
        <div className="max-w-2xl mx-auto antialiased pt-4 relative">
+
           <div className="mb-10 min-h-screen">
-            <h1 className="text-3xl m-5 my-16 text-center text-yellow-600 font-bold">{course?.name}</h1>
+          <div className='m-5 my-16 flex items-center'>
+  <span className="mr-4 cursor-pointer" onClick={() => router.back()}>
+    ←
+  </span>
+  <h1 className="text-3xl text-yellow-600 font-bold text-center justify-center mx-auto">{course?.name}</h1>
+</div>
+
             <TracingBeam className="px-6">
             <ul>
               {Object.entries(course?.data).map(([badge, { title, description }]) => (
@@ -47,7 +52,7 @@ const CoursePage = () => {
             </ul>
             </TracingBeam>
           </div>
-        L</div>
+        </div>
 
         </Layout>
         </div>
