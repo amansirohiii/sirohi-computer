80d642f5f59c759e278c08931fd0aafca381ad65
Aman Sirohi <amansirohi077@gmail.com>
2024-03-28 14:08:40 +0530

    added hover menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import {
   Navbar,
@@ -15,29 +15,36 @@ import {
   Dropdown,
   DropdownMenu,
 } from "@nextui-org/react";
-import { ChevronDown, Lock, Activity, Flash, Server, TagUser, Scale } from "./Icons.jsx";
+import {
+  ChevronDown,
+  Lock,
+  Activity,
+  Flash,
+  Server,
+  TagUser,
+  Scale,
+} from "./Icons.jsx";
 import { Logo } from "./Logo.tsx";
+import { courseMenuItems, mobileMenuItems } from "@/utils/constants.ts";
+import { useState } from "react";
 
 export default function App() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const icons = {
     chevron: <ChevronDown fill="currentColor" size={16} />,
     scale: <Scale className="text-warning" fill="currentColor" size={30} />,
     lock: <Lock className="text-success" fill="currentColor" size={30} />,
-    activity: <Activity className="text-secondary" fill="currentColor" size={30} />,
+    activity: (
+      <Activity className="text-secondary" fill="currentColor" size={30} />
+    ),
     flash: <Flash className="text-primary" fill="currentColor" size={30} />,
     server: <Server className="text-success" fill="currentColor" size={30} />,
     user: <TagUser className="text-danger" fill="currentColor" size={30} />,
   };
 
-  // Define menu items with name and slug
-  const menuItems = [
-    { name: "Home", slug: "/" },
-    { name: "Our Courses", slug: "/courses" },
-    { name: "About Us", slug: "/about" },
-    { name: "Contact Us", slug: "/contact" },
-    { name: "Log Out", slug: "/logout" },
-  ];
+  const [isOpen, setIsOpen] = useState(false);
+  const [timeoutId, setTimeoutId] = useState (null);
+  const delay = 1000;
 
   return (
     <Navbar shouldHideOnScroll onMenuOpenChange={setIsMenuOpen}>
@@ -48,13 +55,15 @@ export default function App() {
         />
         <NavbarBrand>
           <Link color="foreground" href="/">
-            <p className="font-bold text-inherit mx-auto sm:ml-2 text-black sm:text-xl">Sirohi Computer Institute</p>
+            <p className="font-bold text-inherit mx-auto sm:ml-2 text-black sm:text-xl">
+              Sirohi Computer Institute
+            </p>
           </Link>
         </NavbarBrand>
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <Dropdown>
+        <Dropdown isOpen={isOpen}>
           <NavbarItem isActive aria-current="page">
             <DropdownTrigger>
               <Button
@@ -63,6 +72,14 @@ export default function App() {
                 endContent={icons.chevron}
                 radius="sm"
                 variant="light"
+                onMouseEnter={() => {
+                  clearTimeout(timeoutId);
+                  setIsOpen(true);
+                }}
+                onMouseLeave={() => {
+                  const id = setTimeout(() => setIsOpen(false), delay);
+                  setTimeoutId(id);
+                }}
               >
                 Our Courses
               </Button>
@@ -74,19 +91,30 @@ export default function App() {
             itemClasses={{
               base: "gap-4",
             }}
+            onMouseEnter={() => {
+              clearTimeout(timeoutId);
+              setIsOpen(true);
+            }}
+            onMouseLeave={() => {
+              setIsOpen(false);
+            }}
           >
-            {menuItems.map((menuItem) => (
-              <DropdownItem key={menuItem.slug}>
-                <Link href={menuItem.slug}>{menuItem.name}</Link>
+            {courseMenuItems.map((courseMenuItem) => (
+              <DropdownItem key={courseMenuItem.slug}>
+                <Link color="foreground" href={courseMenuItem.slug}>
+                  {courseMenuItem.name}
+                </Link>
               </DropdownItem>
             ))}
           </DropdownMenu>
         </Dropdown>
         <NavbarItem>
-          <Link href="#" >About Us</Link>
+          <Link href="/about">About Us</Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">Contact</Link>
+          <Link color="foreground" href="/contact">
+            Contact
+          </Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
@@ -94,19 +122,27 @@ export default function App() {
           <Link href="#">Login</Link>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="primary" href="/contact" variant="flat">Contact Us</Button>
+          <Button as={Link} color="primary" href="/contact" variant="flat">
+            Contact Us
+          </Button>
         </NavbarItem>
       </NavbarContent>
       <NavbarMenu>
-        {menuItems.map((menuItem, index) => (
+        {mobileMenuItems.map((mobileMenuItem, index) => (
           <NavbarMenuItem key={index}>
             <Link
-              color={index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"}
+              color={
+                index === 2
+                  ? "primary"
+                  : index === mobileMenuItems.length - 1
+                  ? "danger"
+                  : "foreground"
+              }
               className="w-full"
-              href={menuItem.slug}
+              href={mobileMenuItem.slug}
               size="lg"
             >
-              {menuItem.name}
+              {mobileMenuItem.name}
             </Link>
           </NavbarMenuItem>
         ))}
---
+++ src/utils/constants.ts
@@ -0,0 +1,16 @@
+export const mobileMenuItems = [
+    { name: "Home", slug: "/" },
+    { name: "Our Courses", slug: "/courses" },
+    { name: "About Us", slug: "/about" },
+    { name: "Contact Us", slug: "/contact" },
+    { name: "Log Out", slug: "/logout" },
+  ];
+  export const courseMenuItems = [
+    { name: "Web Development", slug: "/courses/web-development" },
+    { name: "O Level", slug: "/courses/o-level" },
+    { name: "Tally", slug: "/courses/tally" },
+    {name: "CCC", slug: "/courses/ccc"},
+    {name: "ECC", slug: "/courses/ecc"},
+    {name: "MS Office", slug: "/courses/ms-office"},
+
+    ];
